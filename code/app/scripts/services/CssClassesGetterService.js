ce8aae29c554e552ba79ff46464a72b8571879f0
Harish Kayarohanam <technews.full@gmail.com>
2014-01-15 13:25:03 +0530

    Added a few more bootstrap css classes from bootstrap class list provided at https://gist.github.com/geksilla/6543145. TODO: did not add button group classes

--- code/app/scripts/services/CssClassesGetterService.js
+++ code/app/scripts/services/CssClassesGetterService.js
@@ -3,17 +3,33 @@
 angular.module('icssApp')
   .service('Cssclassesgetterservice', function Cssclassesgetterservice() {
     // AngularJS will instantiate a singleton by calling "new" on this function
-      
+    // TO BE FIXED : See if you can provide an array as value instead of giving a redundnat key by name "id"
     this.getButtonCssClasses = function() { 
-                                 return  [{
-                                           "id": "btn"
-                                          }, {
-                                           "id": "btn-warning"
-                                          }, {
-                                           "id": "btn-success"
-                                          }, {
-                                           "id": "btn-danger"
-                                          }]
+                                 return [
+                                           {
+                                             "id": "btn"
+                                           }, {
+                                             "id": "btn-danger"
+                                           }, {
+                                             "id": "btn-default"
+                                           }, {
+                                             "id": "btn-info"
+                                           }, {
+                                             "id": "btn-lg"
+                                           }, {
+                                             "id": "btn-link"
+                                           }, {
+                                             "id": "btn-primary"
+                                           }, {
+                                             "id": "btn-success"
+                                           }, {
+                                             "id": "btn-warning"
+                                           }, {
+                                             "id": "btn-xs"
+                                           }, {
+                                             "id": "btn-block"
+                                           }
+                                       ];
                                }
 
      
